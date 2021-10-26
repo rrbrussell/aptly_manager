0413b59d8ead8ade027adce4f1d2633bdc904519
Robert R. Russell <robert@rrbrussell.com>
2021-10-26 17:30:49 -0500

    Added main data types.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
   println!("Hello, world!");
 }
+
+struct DefaultOptions {
+  architectures: Vec<String>,
+  installer: bool,
+  sources: bool,
+  udebs: bool,
+}
+
+struct Distribution {
+  architectures: Vec<String>,
+  components: Vec<String>,
+  installer: bool,
+  sources: bool,
+  udebs: bool,
+  uri: String,
+}
+
+struct Prefix {
+  architectures: Vec<String>,
+  components: Vec<String>,
+  distributions: Vec<String>,
+  installer: bool,
+  sources: bool,
+  udebs: bool,
+  uri: String,
+}
