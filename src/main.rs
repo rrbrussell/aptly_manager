1c2964304c8b91a31a624ca4ac35ed5a71f456cb
Robert R. Russell <robert@rrbrussell.com>
2021-10-26 19:19:07 -0500

    First attempt at a TOML parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+//Copyright (C) 2021 Robert R. Russell
+//This program is free software; you can redistribute it and/or modify it under
+//the terms of the GNU General Public License as published by the Free Software
+//Foundation; version 2.
+//
+//This program is distributed in the hope that it will be useful, but WITHOUT
+//ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License along with
+//this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+//Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -18,6 +32,7 @@ struct CliArguments {
 }
 
 #[allow(dead_code)]
+#[derive(Deserialize, Serialize, Debug)]
 struct DefaultOptions {
   architectures: Vec<String>,
   installer: bool,
--- src/tests/test_defaultoptions.rs
+++ src/tests/test_defaultoptions.rs
@@ -1,4 +1,35 @@
+//Copyright (C) 2021 Robert R. Russell
+//This program is free software; you can redistribute it and/or modify it under
+//the terms of the GNU General Public License as published by the Free Software
+//Foundation; version 2.
+//
+//This program is distributed in the hope that it will be useful, but WITHOUT
+//ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License along with
+//this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+//Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn test_defaultoptions() {
+    let test_string =
+      "architectures: [\"i386\"]\ninstaller = false\nsources = false\nudebs = false\n[debian]\ninstaller=true";
+    let manual = DefaultOptions {
+      architectures: vec![String::from("i386")],
+      installer: false,
+      sources: false,
+      udebs: false,
+    };
+
+    let parsed: DefaultOptions = toml::from_str(test_string).unwrap();
+    assert_eq!(parsed.architectures, manual.architectures);
+    assert_eq!(parsed.installer, manual.installer);
+    assert_eq!(parsed.sources, manual.sources);
+    assert_eq!(parsed.udebs, manual.udebs);
+  }
 }
